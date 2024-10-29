90a847b6ea5b434013c9b3dc885231eb7447488b
dolonche <ivan.pitenin@gmail.com>
2024-10-29 20:12:10 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import relativeLinks from 'astro-relative-links';
 
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [relativeLinks(), tailwind()],
-  output: 'server',
-  adapter: cloudflare(),
 });
