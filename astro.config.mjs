83324afc7b883fd3b9d38a744a21ccd6db2b77af
dolonche <ivan.pitenin@gmail.com>
2024-10-29 20:11:34 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [relativeLinks(), tailwind()],
+  output: 'server',
+  adapter: cloudflare(),
 });
