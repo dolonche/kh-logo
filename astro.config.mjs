c3c6996a18d4c63df7799f3ae21919c10cd52320
dolonche <ivan.pitenin@gmail.com>
2024-11-15 15:16:34 +0900

    fix animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config';
 import relativeLinks from 'astro-relative-links';
 
 import tailwind from '@astrojs/tailwind';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [relativeLinks(), tailwind()],
+  vite: {
+    plugins: [ basicSsl() ]
+  }
 });
